739f1cdcdaeae8a6968a3941c38c481ad1ed9952
Claudio Carnino <claudio@tugulab.org>
2024-02-22 18:30:41 +0000

    feat: supporting digit pattern

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::process;
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
         return input_line.contains(pattern);
+    } else if pattern == r"\d" {
+        return input_line.chars().any(|c| c.is_digit(10));
     } else {
         panic!("Unhandled pattern: {}", pattern)
     }
@@ -26,5 +28,27 @@ fn main() {
         process::exit(0)
     } else {
         process::exit(1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_match_pattern_single_letter() {
+        assert_eq!(match_pattern("hello world", "h"), true);
+        assert_eq!(match_pattern("hello world", "w"), true);
+        assert_eq!(match_pattern("hello world", "f"), false);
+        assert_eq!(match_pattern("hello world", "a"), false);
+    }
+
+    #[test]
+    fn test_match_pattern_single_digit() {
+        assert_eq!(match_pattern("hello world", r"\d"), false);
+        assert_eq!(match_pattern("hello 1 world", r"\d"), true);
+        assert_eq!(match_pattern("2 hello world", r"\d"), true);
+        assert_eq!(match_pattern("hello world 3", r"\d"), true);
+        assert_eq!(match_pattern("Cia0", r"\d"), true);
     }
 }
