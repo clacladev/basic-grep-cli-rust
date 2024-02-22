d38ea77de4ae39cb6d8bab3a208f2e2c7d9d5c01
Claudio Carnino <claudio@tugulab.org>
2024-02-22 18:51:37 +0000

    feat: added support to positive character group

--- src/matcher.rs
+++ src/matcher.rs
@@ -4,13 +4,29 @@ const ALPHANUMERIC_PATTERN: &str = r"\w";
 pub fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
         return input_line.contains(pattern);
-    } else if pattern == DIGITS_PATTERN {
-        return input_line.chars().any(|c| c.is_digit(10));
-    } else if pattern == ALPHANUMERIC_PATTERN {
-        return input_line.chars().any(|c| c.is_alphanumeric() || c == '_');
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
     }
+
+    if pattern == DIGITS_PATTERN {
+        return input_line.chars().any(|c| c.is_digit(10));
+    }
+
+    if pattern == ALPHANUMERIC_PATTERN {
+        return input_line.chars().any(|c| c.is_alphanumeric() || c == '_');
+    }
+
+    if let Some(characters) = get_positive_character_group(pattern) {
+        return input_line.chars().any(|c| characters.contains(&c));
+    }
+
+    panic!("Unhandled pattern: {}", pattern)
+}
+
+fn get_positive_character_group(pattern: &str) -> Option<Vec<char>> {
+    if !pattern.starts_with('[') || !pattern.ends_with(']') {
+        return None;
+    }
+    let characters = pattern.chars().skip(1).take(pattern.len() - 2).collect();
+    Some(characters)
 }
 
 #[cfg(test)]
@@ -48,5 +64,15 @@ mod tests {
         assert_eq!(match_pattern("---", ALPHANUMERIC_PATTERN), false);
         assert_eq!(match_pattern("é", ALPHANUMERIC_PATTERN), true);
         assert_eq!(match_pattern("ç", ALPHANUMERIC_PATTERN), true);
+    }
+
+    #[test]
+    fn test_match_pattern_positive_character_group() {
+        assert_eq!(match_pattern("hello world", "[abc]"), false);
+        assert_eq!(match_pattern("hello world", "[abcd]"), true);
+        assert_eq!(match_pattern("hello world", "[etz]"), true);
+        assert_eq!(match_pattern("hello world", "[cd]"), true);
+        assert_eq!(match_pattern("hello world", "[abctyj]"), false);
+        assert_eq!(match_pattern("hello world", "[abctyjh]"), true);
     }
 }
